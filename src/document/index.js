ddd74bbed1f500296b57bc54d3f20ac34633a7f8
DougA <douganderson444@gmail.com>
2020-11-22 13:03:38 -0500

    linting

--- src/document/index.js
+++ src/document/index.js
@@ -1,111 +1,111 @@
-import { omitBy, isArray, isUndefined } from 'lodash';
-import service from './service';
-import publicKey from './publicKey';
-import authentication from './authentication';
-import { generateDocument, isEquivalentId, assertDocument } from './utils';
+import { omitBy, isArray, isUndefined } from 'lodash'
+import service from './service'
+import publicKey from './publicKey'
+import authentication from './authentication'
+import { generateDocument, isEquivalentId, assertDocument } from './utils'
 
 class Document {
     #content;
 
-    constructor(content) {
-        this.#content = {
-            publicKey: [],
-            authentication: [],
-            service: [],
-            ...content,
-        };
+    constructor (content) {
+      this.#content = {
+        publicKey: [],
+        authentication: [],
+        service: [],
+        ...content
+      }
     }
 
-    getContent() {
-        return omitBy(this.#content, (prop) => isUndefined(prop) || (isArray(prop) && prop.length === 0));
+    getContent () {
+      return omitBy(this.#content, (prop) => isUndefined(prop) || (isArray(prop) && prop.length === 0))
     }
 
-    addPublicKey(props, options) {
-        const { idPrefix } = { ...options };
+    addPublicKey (props, options) {
+      const { idPrefix } = { ...options }
 
-        props.id = publicKey.createId(this.#content.id, props.id, { prefix: idPrefix });
-        props.controller = props.controller || this.#content.id;
+      props.id = publicKey.createId(this.#content.id, props.id, { prefix: idPrefix })
+      props.controller = props.controller || this.#content.id
 
-        publicKey.assert(props, this.#content.publicKey);
+      publicKey.assert(props, this.#content.publicKey)
 
-        this.#content.publicKey.push(props);
-        this.#refreshUpdated();
+      this.#content.publicKey.push(props)
+      this.#refreshUpdated()
 
-        return props;
+      return props
     }
 
-    revokePublicKey(id) {
-        const filter = this.#content.publicKey.filter((key) => !isEquivalentId(key.id, id, publicKey.separator));
+    revokePublicKey (id) {
+      const filter = this.#content.publicKey.filter((key) => !isEquivalentId(key.id, id, publicKey.separator))
 
-        if (this.#content.publicKey.length === filter.length) {
-            return;
-        }
+      if (this.#content.publicKey.length === filter.length) {
+        return
+      }
 
-        this.removeAuthentication(id);
+      this.removeAuthentication(id)
 
-        this.#content.publicKey = filter;
-        this.#refreshUpdated();
+      this.#content.publicKey = filter
+      this.#refreshUpdated()
     }
 
-    addAuthentication(auth) {
-        const key = this.#content.publicKey.find((pk) => isEquivalentId(pk.id, auth, publicKey.separator)) || {};
+    addAuthentication (auth) {
+      const key = this.#content.publicKey.find((pk) => isEquivalentId(pk.id, auth, publicKey.separator)) || {}
 
-        authentication.assert(key.id, this.#content.authentication);
+      authentication.assert(key.id, this.#content.authentication)
 
-        this.#content.authentication.push(key.id);
-        this.#refreshUpdated();
+      this.#content.authentication.push(key.id)
+      this.#refreshUpdated()
 
-        return key.id;
+      return key.id
     }
 
-    removeAuthentication(id) {
-        const filter = this.#content.authentication.filter((auth) => !isEquivalentId(id, authentication.parseId(auth), publicKey.separator));
+    removeAuthentication (id) {
+      const filter = this.#content.authentication.filter((auth) => !isEquivalentId(id, authentication.parseId(auth), publicKey.separator))
 
-        if (this.#content.authentication.length === filter.length) {
-            return;
-        }
+      if (this.#content.authentication.length === filter.length) {
+        return
+      }
 
-        this.#content.authentication = filter;
-        this.#refreshUpdated();
+      this.#content.authentication = filter
+      this.#refreshUpdated()
     }
 
-    addService(props, options) {
-        const { idPrefix } = { ...options };
+    addService (props, options) {
+      const { idPrefix } = { ...options }
 
-        props.id = service.createId(this.#content.id, props.id, { prefix: idPrefix });
+      props.id = service.createId(this.#content.id, props.id, { prefix: idPrefix })
 
-        service.assert(props, this.#content.service);
+      service.assert(props, this.#content.service)
 
-        this.#content.service.push(props);
-        this.#refreshUpdated();
+      this.#content.service.push(props)
+      this.#refreshUpdated()
 
-        return props;
+      return props
     }
 
-    removeService(id) {
-        const filter = this.#content.service.filter((srvc) => !isEquivalentId(srvc.id, id, service.separator));
+    removeService (id) {
+      const filter = this.#content.service.filter((srvc) => !isEquivalentId(srvc.id, id, service.separator))
 
-        if (this.#content.service.length === filter.length) {
-            return;
-        }
+      if (this.#content.service.length === filter.length) {
+        return
+      }
 
-        this.#content.service = filter;
-        this.#refreshUpdated();
+      this.#content.service = filter
+      this.#refreshUpdated()
     }
 
     #refreshUpdated = () => {
-        this.#content.updated = new Date().toISOString();
+      this.#content.updated = new Date().toISOString()
     }
 }
 
 const createDocument = (did, content) => {
-    if (!content) {
-        content = generateDocument(did);
-    }
+  if (!content) {
+    content = generateDocument(did)
+  }
 
-    return new Document(content);
-};
+  return new Document(content)
+}
 
-export { assertDocument };
+export { assertDocument }
 
-export default createDocument;
+export default createDocument
